8a5229215df262ec7900621a692b188416756d6c
John Rambo <99561247+LoneWolfJJR@users.noreply.github.com>
2022-02-07 13:37:56 +0200

    Add support for MOM two-way single metro tracks.

--- SingleTrackAI/TrackInfo.cs
+++ SingleTrackAI/TrackInfo.cs
@@ -7,6 +7,17 @@ namespace SingleTrackAI
 {
     public struct TrackInfo
     {
+        // ReSharper disable once IdentifierTypo
+        // ReSharper disable once InconsistentNaming
+        // TODO: Find a way to dynamically detect these tracks to avoid hardcoding them.
+        private static readonly HashSet<string> MOM_TWOWAY_TRACKS = new HashSet<string>
+        {
+            "Metro Track Ground Small Two-Way",
+            "Metro Track Ground Small Two-Way NoBar",
+            "Steel Metro Track Ground Small Two-Way",
+            "Steel Metro Track Ground Small Two-Way NoBar",
+        };
+
         public readonly NetInfo NetInfo;
         public readonly VehicleInfo.VehicleType VehicleType;
         public readonly int Tracks;
@@ -16,7 +27,21 @@ namespace SingleTrackAI
         public readonly int Tracks2Way;
         public readonly int Platforms;
 
-        public bool IsSingleTwoWayTrack => Tracks == 1 && Tracks2Way == 1;
+        public bool IsSingleTwoWayTrack => IsGenericSingleTwoWayTrack || IsMetroOverhaulSingleTwoWayTrack;
+
+        public bool IsGenericSingleTwoWayTrack => Tracks == 1 && Tracks2Way == 1;
+
+        // MOM two-way single tracks are actually double tracks in different directions,
+        // but rendered overlapping, so it appears there is only a single track.
+        // This makes is quite difficult (impossible?) to detect using the network info lanes,
+        // as its lanes are identical to the regular two-way double track.
+        public bool IsMetroOverhaulSingleTwoWayTrack =>
+            Tracks == 2 &&
+            Tracks1Way == 2 &&
+            Tracks1WayForward == 1 &&
+            Tracks1WayBackward == 1 &&
+            VehicleType == VehicleInfo.VehicleType.Metro &&
+            MOM_TWOWAY_TRACKS.Contains(NetInfo.name);
 
         public bool IsStationTrack => Platforms != 0 && (Tracks == 1 || Tracks == 2); // TODO: Maybe check if Tracks > 0? This will include quad-station tracks.
 
