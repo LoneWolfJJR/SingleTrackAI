222a2d08ddb3b098cf0332fca35882f835c6a211
John Rambo <99561247+LoneWolfJJR@users.noreply.github.com>
2022-02-07 13:35:34 +0200

    Extract segment track info into a separate object.

---
+++ SingleTrackAI/TrackInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SingleTrackAI
+{
+    public struct TrackInfo
+    {
+        public readonly NetInfo NetInfo;
+        public readonly VehicleInfo.VehicleType VehicleType;
+        public readonly int Tracks;
+        public readonly int Tracks1Way;
+        public readonly int Tracks2Way;
+        public readonly int Platforms;
+
+        public bool IsSingleTwoWayTrack => Tracks == 1 && Tracks2Way == 1;
+
+        public bool IsStationTrack => Platforms != 0 && (Tracks == 1 || Tracks == 2); // TODO: Maybe check if Tracks > 0? This will include quad-station tracks.
+
+        public bool IsSingleStationTrack => Platforms != 0 && Tracks == 1;
+
+        public bool IsDoubleStationTrack => Platforms != 0 && Tracks == 2;
+
+        public TrackInfo(
+            NetInfo netInfo,
+            VehicleInfo.VehicleType vehicleType,
+            int tracks,
+            int tracks1Way,
+            int tracks2Way,
+            int platforms)
+        {
+            NetInfo = netInfo;
+            VehicleType = vehicleType;
+            Tracks = tracks;
+            Tracks1Way = tracks1Way;
+            Tracks2Way = tracks2Way;
+            Platforms = platforms;
+        }
+    }
+}
